6748857d0da438e292b8f06bd738f138999ce009
Shantamma <shantammab12@gmail.com>
2021-03-23 17:55:11 +0530

    line 1 changed

--- my-dummy-project/src/Hello.java
+++ my-dummy-project/src/Hello.java
@@ -2,7 +2,7 @@
 public class Hello {
 	public void method() {
 		System.out.println("Hello everyone");
-		System.out.println("Line 1");
+		System.out.println("Line 1 is not ok");
 		System.out.println("Line 2");
 		System.out.println("Line 3");
 	}
