f9b3a67c9588723cb060db7e21f83804bbfeae42
Shantamma <shantammab12@gmail.com>
2021-03-23 17:41:59 +0530

    line 2 and 3

--- my-dummy-project/src/Hello.java
+++ my-dummy-project/src/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
 	public void method() {
 		System.out.println("Hello everyone");
 		System.out.println("Line 1");
+		System.out.println("Line 2");
+		System.out.println("Line 3");
 	}
 
 }
